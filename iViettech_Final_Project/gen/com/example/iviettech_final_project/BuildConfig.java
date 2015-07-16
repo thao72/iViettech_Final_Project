1f91fade904b961a8fc189af9c6d5b4e015afed6
quanghuyconqueror <quanghuyconqueror@gmail.com>
2015-07-17 00:35:50 +0700

    Sdsadsa

---
+++ iViettech_Final_Project/gen/com/example/iviettech_final_project/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.iviettech_final_project;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}

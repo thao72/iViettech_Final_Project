a485ddc76b1d68ab18e9d91396abcbb112f8d6dd
thao72 <thaolqdn@gmail.com>
2015-07-25 20:11:06 +0700

    ytyt

--- iViettech_Final_Project/src/com/example/iviettech_final_project/MainActivity.java
+++ iViettech_Final_Project/src/com/example/iviettech_final_project/MainActivity.java
@@ -9,10 +9,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.os.Build;
 
 public class MainActivity extends Activity {
-
+	Button bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
